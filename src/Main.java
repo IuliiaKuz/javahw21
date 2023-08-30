ecf28beeee0d937f92cda02ccba244902cd4b082
iuliia <juliyakuzya@gmail.com>
2023-08-30 19:15:43 +0300

    add gitignore , src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 76_894;
+
+        int bonus = price / 20;
+        System.out.println("Бонус " + bonus);
+
+    }
+}
